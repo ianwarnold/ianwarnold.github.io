8f303ba3dd80f53b060d2ab07c5173debb4d1953
Ian Arnold <ianwarnold@gmail.com>
2024-05-17 09:22:10 -0600

    Changing blog title, updating styling

--- jekyll.sh
+++ jekyll.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-/home/ianwarnold/.local/share/gem/ruby/3.0.0/bin/jekyll $@
+# /home/ianwarnold/.local/share/gem/ruby/3.0.0/bin/jekyll $@
+/home/ianwarnold/.gem/ruby/3.0.0/bin/jekyll $@
